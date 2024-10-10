c5dcd35879e420d4de58aeb4ea18ad66e04a8304
Peroze <konper98@gmail.com>
2024-10-10 03:00:30 +0300

    Improvemets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Menubar from "./Components/menubar.jsx";
+import Menubar from "./Components/MenuBar.jsx";
 import Home from "./Components/Home.jsx";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Start from "./Components/Start.jsx";
